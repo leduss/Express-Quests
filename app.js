7aaa8a68c6f6c84058a1756d089b95beab33396f
Julien DUSSART <julien.dussart59@gmail.com>
2022-11-15 09:36:07 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.APP_PORT ?? 5001;
+
+const welcome = (req, res) => {
+  res.send('Welcome to my favourite movie list');
+};
+
+app.get('/', welcome);
+
+const movieHandlers = require('./movieHandlers');
+const usersHandlers = require('./usersHandlers');
+
+app.get('/api/movies', movieHandlers.getMovies);
+app.get('/api/movies/:id', movieHandlers.getMovieById);
+app.post('/api/movies', movieHandlers.postMovie);
+app.put('/api/movies/:id', movieHandlers.updateMovie);
+
+app.get('/api/users', usersHandlers.getUsers);
+app.get('/api/users/:id', usersHandlers.getUserById);
+app.post('/api/users', usersHandlers.postUser);
+app.put('/api/users/:id', usersHandlers.updateUser);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error('Something bad happened');
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
